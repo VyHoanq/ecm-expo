4b1dcb964b359526bde72ede6f599df9499b7a23
VyHoanq <vyhung1122@gmail.com>
2026-03-24 11:47:51 +0700

    added context for wishlist and cart feature

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,19 @@
+import { CartProvider } from '@/context/CartContext';
+import { WishlistProvider } from '@/context/WishlistContext';
 import '@/global.css';
 import { Stack } from "expo-router";
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 
 export default function RootLayout() {
-  return <Stack screenOptions={{ headerShown: false }} />;
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <CartProvider>
+        <WishlistProvider>
+          <Stack screenOptions={{ headerShown: false }} />;
+        </WishlistProvider>
+      </CartProvider>
+    </GestureHandlerRootView>
+  )
+
 }
